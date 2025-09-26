302780a71bef9d4de31179e292d2dead8bc1ccc8
Joshua Rogers <MegaManSec@users.noreply.github.com>
2025-09-26 23:44:16 +0200

    update: install.sh

--- macos/Users/jrogers/install.sh
+++ macos/Users/jrogers/install.sh
@@ -1,6 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
-brew install bash-completion@2 brew-cask-completion dnscrypt-proxy grc mpv nano nanorc coreutils curls dnscrypt-proxy dos2unix gawk git gnu-sed grc grep htop mpv mtr nano nanorc nmap p7zip parallel pnpm pstree ripgrep wget
+brew install bash-completion@2 brew-cask-completion dnscrypt-proxy grc mpv nano nanorc coreutils curl dos2unix gawk git gnu-sed grep htop mtr nmap p7zip parallel pnpm pstree ripgrep wget
 chsh -s /opt/homebrew/bin/bash jrogers
 
 echo "Generating PERSONAL-RANDOM SSH KEY"

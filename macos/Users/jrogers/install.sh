fa5c5d6b69a46c4938e77d2a3bcc610281c3e72c
Joshua Rogers <MegaManSec@users.noreply.github.com>
2025-11-11 09:36:50 +0100

    add: install background image

--- macos/Users/jrogers/install.sh
+++ macos/Users/jrogers/install.sh
@@ -1,7 +1,12 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
-brew install bash-completion@2 brew-cask-completion dnscrypt-proxy grc mpv nano nanorc coreutils curl dos2unix gawk git gnu-sed grep htop mtr nmap p7zip parallel pnpm pstree ripgrep wget
-chsh -s /opt/homebrew/bin/bash jrogers
+brew install bash-completion@2 brew-cask-completion dnscrypt-proxy grc mpv nano nanorc coreutils curl dos2unix gawk git gnu-sed grep htop mtr nmap p7zip parallel pnpm pstree ripgrep wget aspell
+sudo chsh -s /opt/homebrew/bin/bash jrogers
+
+osascript -e "
+tell application \"Finder\"
+  set desktop picture to POSIX file wallpaper.jpg
+end tell"
 
 echo "Generating PERSONAL-RANDOM SSH KEY"
 ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519-personal-random
